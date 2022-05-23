df6bdc8dbb467090adc48ebfe780b569a0816feb
LeoPMD <leo.pellegrino2b@gmail.com>
2022-05-23 10:37:20 +0200

    Ajout de la classe Utilisateur

---
+++ src/main/java/com/inti/SpringBootTD3BisSecurity/model/Utilisateur.java
@@ -0,0 +1,34 @@
+package com.inti.SpringBootTD3BisSecurity.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.NonNull;
+import lombok.RequiredArgsConstructor;
+
+@Entity
+@Table
+@Data
+@NoArgsConstructor @RequiredArgsConstructor @AllArgsConstructor
+public class Utilisateur {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int id;
+	private @NonNull String nom;
+	private String prenom;
+	@Column(unique = true)
+	private String username;
+	@Column(unique = true)
+	private String email;
+	private String mdp;
+	private String telephone;
+	
+}
